df817d69a2f32225ff440b8895b4a1e6e8ed659c
chillrod <rodramone@live.com>
2021-10-28 20:38:10 -0300

    #feat - handling user friendly error, separating concerns of application

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -10,7 +10,13 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-    return this.coffees.find((coffee) => coffee.id === +id);
+    const coffee = this.coffees.find((coffee) => coffee.id === +id);
+
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+
+    return coffee;
   }
 
   create(createCoffeeDto: any) {
